4e29338f9d7d50ed393a89f2b08aa6f8fdf8fd62
Brendan Gasparin <brendan.gasparin@gmail.com>
2024-05-06 21:59:27 +1000

    Print blank grid

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_grid(void);
+
 int main(void)
 {
     system("clear");
     printf("Commencing C game...\n");
+
+    print_grid();
+
     return 0;
+}
+
+void print_grid(void) {
+    // PRINT BLANK GRID
+    printf("+---+---+---+\n");
+    printf("|   |   |   |\n");
+    printf("+---+---+---+\n");
+    printf("|   |   |   |\n");
+    printf("+---+---+---+\n");
+    printf("|   |   |   |\n");
+    printf("+---+---+---+\n");
 }
