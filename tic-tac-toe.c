25edd29b623e4172a613e9d4ce388c81fa6fcd1c
Brendan Gasparin <brendan.gasparin@gmail.com>
2024-05-07 11:15:41 +1000

    Implement simple user input

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 
 void print_grid(void);
+char *get_move(char *prompt, int characters);
 
 int main(void)
 {
     system("clear");
     printf("TIC-TAC-TOE\n");
+    printf("Enter 1 or 2 for players.\n");
     printf("-----------\n\n");
     print_grid();
     printf("\n");
 
-    char *no_of_players = get_input("1 or 2 Players: ", 1);
+    char *no_of_players = get_move("1 or 2 Players: ", 1);
+
+    free(no_of_players);
 
     return 0;
 }
@@ -28,12 +32,22 @@ void print_grid(void) {
     printf("   +---+---+---+\n");
 }
 
-char * get_input(char *prompt, int no_of_chars) {
+char *get_move(char *prompt, int no_of_chars) {
     int count = 0;
-    char *input = malloc(no_of_chars * sizeof(char));
+    int c;
+    char *input = (char *)malloc((no_of_chars + 1) * sizeof(char));
+    if(input == NULL) {
+        printf("Memory allocaiton failed.\n");
+        exit(EXIT_FAILURE);
+    }
 
-    while ()) {
-        input[count] == c.getChar();
+    printf("%s", prompt);
+
+    while ((c = getchar()) != EOF && c != '\n' && count < no_of_chars) {
+        input[count] = c;
         count++;
     }
+    input[count] == '\0';
+
+    return input;
 }
