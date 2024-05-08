4634d430e715d5453b5ada9f5fb814467ffd64a4
Brendan Gasparin <brendan.gasparin@gmail.com>
2024-05-08 17:20:02 +1000

    Refactor code

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -2,28 +2,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_grid(void);
+#define BOARD_SIZE  3
+
+int board[BOARD_SIZE][BOARD_SIZE];
+
 char *get_move(char *prompt, int characters);
+int get_no_of_players(char *prompt);
+void print_board(void);
+void print_grid(void);
+void print_header(void);
 
 int main(void)
 {
-    system("clear");
-    printf("TIC-TAC-TOE\n");
-    printf("-----------\n");
+    // initialise board
+    for (int i = 0; i < BOARD_SIZE; i++)
+        for (int j = 0; j < BOARD_SIZE; j++)
+            board[i][j] = ' ';
+
+    print_header();
     printf("Enter 1 or 2 for players.\n\n");
-    print_grid();
+    print_board();
     printf("\n");
 
     char* no_of_players;
     int players = -1;
 
-    players = get_no_of_players("1 or 2 Players (q to exit):");
-
-    // TODO: The game stuff
-
+    players = get_no_of_players("1 or 2 Players (q to exit): ");
     free(no_of_players);
 
+    // TODO: The game stuff
+    
+
     return 0;
+}
+
+char *get_move(char *prompt, int no_of_chars) {
+    int count = 0;
+    int c;
+    char *input = (char *)malloc((no_of_chars + 1) * sizeof(char));
+    if(input == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%s", prompt);
+
+    while ((c = getchar()) != EOF && c != '\n' && count < no_of_chars) {
+        input[count] = c;
+        count++;
+    }
+    input[count] == '\0';
+
+    return input;
 }
 
 int get_no_of_players(char *prompt) {
@@ -46,35 +76,20 @@ int get_no_of_players(char *prompt) {
 
     return players;
 }
-
-void print_grid(void) {
-    // PRINT BLANK GRID
+void print_board(void) {
+    // PRINT GRID
     printf("     a   b   c\n");
     printf("   +---+---+---+\n");
-    printf(" 1 |   |   |   |\n");
+    printf(" 1 | %c | %c | %c |\n", board[0][0], board[0][1], board[0][2]);
     printf("   +---+---+---+\n");
-    printf(" 2 |   |   |   |\n");
+    printf(" 2 | %c | %c | %c |\n", board[1][0], board[0][1], board[0][2]);
     printf("   +---+---+---+\n");
-    printf(" 3 |   |   |   |\n");
+    printf(" 3 | %c | %c | %c |\n", board[2][0], board[2][1], board[2][2]);
     printf("   +---+---+---+\n");
 }
 
-char *get_move(char *prompt, int no_of_chars) {
-    int count = 0;
-    int c;
-    char *input = (char *)malloc((no_of_chars + 1) * sizeof(char));
-    if(input == NULL) {
-        printf("Memory allocation failed.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("%s", prompt);
-
-    while ((c = getchar()) != EOF && c != '\n' && count < no_of_chars) {
-        input[count] = c;
-        count++;
-    }
-    input[count] == '\0';
-
-    return input;
+void print_header(void) {
+    system("clear");
+    printf("TIC-TAC-TOE\n");
+    printf("-----------\n");
 }
