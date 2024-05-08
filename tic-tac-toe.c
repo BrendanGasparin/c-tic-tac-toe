c6aa8a38f44fc5bfc0a953daebf8c481a06d5c76
Brendan Gasparin <brendan.gasparin@gmail.com>
2024-05-08 17:39:35 +1000

    Implement random selection of starting player in single player game

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -1,6 +1,8 @@
 #include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define BOARD_SIZE  3
 
@@ -9,11 +11,13 @@ int board[BOARD_SIZE][BOARD_SIZE];
 char *get_move(char *prompt, int characters);
 int get_no_of_players(char *prompt);
 void print_board(void);
-void print_grid(void);
 void print_header(void);
+void single_player_game(void);
 
 int main(void)
 {
+    int players;
+
     // initialise board
     for (int i = 0; i < BOARD_SIZE; i++)
         for (int j = 0; j < BOARD_SIZE; j++)
@@ -22,16 +26,22 @@ int main(void)
     print_header();
     printf("Enter 1 or 2 for players.\n\n");
     print_board();
-    printf("\n");
 
-    char* no_of_players;
-    int players = -1;
-
+    // Get number of players
     players = get_no_of_players("1 or 2 Players (q to exit): ");
-    free(no_of_players);
 
-    // TODO: The game stuff
-    
+    // If 1 player then warn the user and exit
+    if (players == 1) {
+        print_header();
+        printf("CPU player not implemented. Try a 2 player game.\n\n");
+        print_board();
+        printf("CPU player not implemented. Try a 2 player game.\nExiting Tic-Tac-Toe.\n");
+        exit(0);
+    }
+
+    // Start game loop
+    if (players == 2)
+        single_player_game();
 
     return 0;
 }
@@ -69,15 +79,18 @@ int get_no_of_players(char *prompt) {
             players = 2;
         else if (tolower(no_of_players[0]) == 'q')
         {
+            free(no_of_players);
             printf("Exiting program.\n");
             exit(0);
         }
     }
 
+    free(no_of_players);
+
     return players;
 }
 void print_board(void) {
-    // PRINT GRID
+    // PRINT BOARD
     printf("     a   b   c\n");
     printf("   +---+---+---+\n");
     printf(" 1 | %c | %c | %c |\n", board[0][0], board[0][1], board[0][2]);
@@ -85,11 +98,27 @@ void print_board(void) {
     printf(" 2 | %c | %c | %c |\n", board[1][0], board[0][1], board[0][2]);
     printf("   +---+---+---+\n");
     printf(" 3 | %c | %c | %c |\n", board[2][0], board[2][1], board[2][2]);
-    printf("   +---+---+---+\n");
+    printf("   +---+---+---+\n\n");
 }
 
 void print_header(void) {
     system("clear");
     printf("TIC-TAC-TOE\n");
     printf("-----------\n");
+}
+
+void single_player_game(void) {
+    unsigned short current_player;
+    bool game_over = false;
+
+    // RANDOMLY SELECT A STARTING PLAYER
+    // Seed the random number generator using the current time
+    srand(time(NULL));
+    // Generate a random number between 0 and RAND_MAX
+    int random_number = rand();
+    // 50-50 chance for each player
+    if (random_number % 2 == 0)
+        current_player = 1;
+    else
+        current_player = 2;
 }
